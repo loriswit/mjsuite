3741ca6c70ff622ec22a52ce32fa6bfc10cc4422
Loris Witschard <loris.wit@gmail.com>
2023-06-22 17:29:00 +0200

    feat(plot): add plot command

---
+++ src/commands/plot.ts
@@ -0,0 +1,39 @@
+import {plot, Plot as PlotData} from "nodeplotlib"
+import {Stats} from "./benchmark.js"
+
+interface PlotEntry {
+    title: string
+    plot: PlotData[]
+}
+
+export class Plot {
+    private readonly plots: PlotEntry[]
+
+    public constructor(benchmark: Stats) {
+        const stats = [
+            {title: "Run time (ms)", id: "runTime", scale: 1 / 1000000},
+            {title: "Memory usage (MB)", id: "maxMemory", scale: 1 / 1000000},
+            {title: "Instructions count", id: "instructions", scale: 1},
+            {title: "Branches", id: "branches", scale: 1},
+            {title: "Branch misses", id: "branchMisses", scale: 1},
+            {title: "Page faults", id: "pageFaults", scale: 1},
+        ]
+
+        this.plots = stats.map(({title, id, scale}) => ({
+            title,
+            plot: Object.entries(benchmark).map(([workloadId, stats]) => {
+                return {
+                    name: workloadId,
+                    x: Object.keys(stats),
+                    y: Object.values(stats).map(stat => stat[id] * scale),
+                    type: "bar",
+                }
+            }),
+        }))
+    }
+
+    public show() {
+        for (const entry of this.plots)
+            plot(entry.plot, {title: {text: entry.title}})
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import {OptionValues, program} from "commander"
+import {readFile, writeFile} from "fs/promises"
 
 import "./utils/globals.js"
 import {logger, LogLevel} from "./utils/logger.js"
 import {Engine, EngineId} from "./commands/engine.js"
 import {Workload, WorkloadId} from "./commands/workload.js"
 import {Benchmark} from "./commands/benchmark.js"
-import {writeFile} from "fs/promises"
+import {Plot} from "./commands/plot.js"
 import {timestamp} from "./utils/helpers.js"
+
+program
+    .name("mjsuite")
+    .description("μJSuite is a tool to benchmark IoT-friendly JavaScript engines")
+    .version(PKG_VERSION)
+    .option("-v, --verbose", "print additional details for debugging purpose")
+    .configureOutput({
+        writeOut: str => logger.info(str),
+        writeErr: str => logger.error(str),
+    })
 
 program
     .command("benchmark").alias("bm")
@@ -16,6 +27,7 @@ program
     .option("-w, --workload <workloads...>", "the workload(s) to run (default: all)")
     .option("-e, --engine <engines...>", "the engine(s) to use (default: all)")
     .option("-o, --output <filename>", "the output file that will store the results")
+    .option("-p, --plot", "displays plots once the benchmark is generated")
     .action(actionWrapper(async (options: OptionValues) => {
         const engineIds = options.engine ?? await Engine.getAllIds()
         const engines = engineIds.map((id: EngineId) => new Engine(id))
@@ -30,18 +42,12 @@ program
         const data = JSON.stringify(results, null, 4)
         await writeFile(filename, data)
         logger.info("Saved results to " + filename)
+
+        if (options.plot) {
+            logger.info("-------------------------------")
+            new Plot(results).show()
+        }
     }))
-
-program
-    .name("mjsuite")
-    .description("μJSuite is a tool to benchmark IoT-friendly JavaScript engines")
-    .version(PKG_VERSION)
-    .option("-v, --verbose", "print additional details for debugging purpose")
-    .configureOutput({
-        writeOut: str => logger.info(str),
-        writeErr: str => logger.error(str),
-    })
-
 
 const engineCmd = program
     .command("engine")
@@ -59,7 +65,7 @@ engineCmd
     .description("Download and build (or rebuild) one of the available JavaScript engines")
     .summary("setup an engine")
     .argument("<engine>", "the engine to setup")
-    .action(actionWrapper(async (engineId) => new Engine(engineId).setup()))
+    .action(actionWrapper(async engineId => new Engine(engineId).setup()))
 
 
 const workloadCmd = program
@@ -72,6 +78,17 @@ workloadCmd
     .description("List all available workloads")
     .summary("list available workloads")
     .action(actionWrapper(async () => logger.table(await Workload.listAll())))
+
+program
+    .command("plot")
+    .description("Generate and display plots for a benchmark.")
+    .summary("draw plots")
+    .argument("<benchmark>", "a benchmark JSON file")
+    .action(actionWrapper(async benchmarkFile => {
+        const json = await readFile(benchmarkFile, "utf-8")
+        const benchmark = JSON.parse(json)
+        new Plot(benchmark).show()
+    }))
 
 program.on("option:verbose", () => logger.logLevel = LogLevel.DEBUG)
 
